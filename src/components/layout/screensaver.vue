2036186f0ebd0ae6438025e8a8d0345134530290
codesinging <codesinging@gmail.com>
2022-03-31 16:16:01 +0800

    增加组件：Screensaver

---
+++ src/components/layout/screensaver.vue
@@ -0,0 +1,21 @@
+<template>
+    <div v-if="screensaver.visible" class="h-screen flex flex-col items-center justify-center">
+        <div class="h-16">
+            <img src="../../assets/images/logo.svg" alt="PinFan Logo" class="h-16 w-16 animate-pulse">
+        </div>
+        <div v-if="screensaver.text" class="mt-2">
+            <slot name="text">{{ screensaver.text }}</slot>
+        </div>
+    </div>
+    <slot v-else></slot>
+</template>
+
+<script setup>
+import {useScreensaver} from "../../states/screensaver";
+
+const screensaver = useScreensaver()
+</script>
+
+<style scoped>
+
+</style>
