14be54d0890eb483ff9cc2efa2418987a817a308
codesinging <codesinging@gmail.com>
2022-03-31 10:50:38 +0800

    增加请求状态的状态管理

---
+++ src/states/status.js
@@ -0,0 +1,25 @@
+import {defineStore} from "pinia";
+
+export const useStatus = defineStore('status', {
+    state: () => ({
+        status: {
+            loading: false,
+        }
+    }),
+
+    getters: {
+        get: state => {
+            return (...keys) => !!state.status[keys.join('_')]
+        },
+    },
+
+    actions: {
+        set(...keys) {
+            this.status[keys.join('_')] = true
+        },
+
+        unset(...keys) {
+            this.status[keys.join('_')] = false
+        }
+    },
+})
