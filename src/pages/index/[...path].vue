121b8206b7ed2214c5bf59e7568c144ff59c8202
codesinging <codesinging@gmail.com>
2022-04-03 19:20:48 +0800

    增加404页面

---
+++ src/pages/index/[...path].vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex items-center justify-center h-64 text-gray-500">Page [{{ routePath }}] Not Found</div>
+</template>
+
+<script setup>
+import {inject} from "vue";
+
+const routePath = inject('originalPath')
+
+</script>
+
+<style scoped>
+
+</style>
