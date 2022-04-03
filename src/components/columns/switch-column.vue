79df99a03b6b68f08ea80ce3b901d12fd6a02d90
codesinging <codesinging@gmail.com>
2022-04-03 11:21:01 +0800

    增加表格列自定义组件：switch-column

---
+++ src/components/columns/switch-column.vue
@@ -0,0 +1,36 @@
+<template>
+    <el-table-column :label="label" :prop="prop" align="center" width="100">
+        <template #default="scope">
+            <el-switch
+                v-model="scope.row[prop]"
+                @change="change(scope)"
+                :loading="cellStatus(scope)"
+                size="small"
+                inline-prompt
+                :active-icon="Check"
+                :inactive-icon="Close"
+            ></el-switch>
+        </template>
+    </el-table-column>
+</template>
+
+<script setup>
+import {inject} from "vue";
+import {Check, Close} from "@icon-park/vue-next";
+
+defineProps({
+    label: String,
+    prop: String,
+})
+
+const update = inject('update')
+const cellStatus = inject('cellStatus')
+
+const change = scope => {
+    update(scope)
+}
+</script>
+
+<style scoped>
+
+</style>
