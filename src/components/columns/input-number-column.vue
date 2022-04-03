e491b2bcc001c623efb219d47d8b2d53a3f7a4e5
codesinging <codesinging@gmail.com>
2022-04-03 11:20:47 +0800

    增加表格列自定义组件：input-number-column

---
+++ src/components/columns/input-number-column.vue
@@ -0,0 +1,32 @@
+<template>
+    <el-table-column :label="label" :prop="prop" align="center">
+        <template #default="scope">
+            <el-input-number
+                v-model="scope.row[prop]"
+                @change="change(scope)"
+                v-loading="cellStatus(scope)"
+                size="small"
+            ></el-input-number>
+        </template>
+    </el-table-column>
+</template>
+
+<script setup>
+import {inject} from "vue";
+
+defineProps({
+    label: String,
+    prop: String,
+})
+
+const update = inject('update')
+const cellStatus = inject('cellStatus')
+
+const change = scope => {
+    update(scope)
+}
+</script>
+
+<style scoped>
+
+</style>
