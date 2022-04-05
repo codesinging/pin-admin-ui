3c16a705604b2ed2eafc15228708669f93bdae29
codesinging <codesinging@gmail.com>
2022-04-05 23:29:11 +0800

    完善角色管理页面

--- src/pages/index/roles.vue
+++ src/pages/index/roles.vue
@@ -5,15 +5,21 @@
         </template>
 
         <template #form-items="{data}">
-            <el-form-item label="名称" prop="name">
+            <el-form-item label="名称" prop="name" :rules="{required:true, message:'名称不能为空'}">
                 <el-input v-model="data.name"></el-input>
             </el-form-item>
+            <el-form-item label="描述" prop="description">
+                <el-input v-model="data.description"></el-input>
+            </el-form-item>
+        </template>
+
+        <template #view-items="{data}">
+            <el-descriptions-item label="描述">{{ data.description }}</el-descriptions-item>
         </template>
     </model-view>
 </template>
 
 <script setup>
-
 import ModelView from "../../components/views/model-view.vue";
 </script>
 
