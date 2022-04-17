90e24b88512848907d060e5c07260d2e4c5d9596
codesinging <codesinging@gmail.com>
2022-04-17 14:34:17 +0800

    增加合并表格列

---
+++ src/components/columns/joined-column.vue
@@ -0,0 +1,25 @@
+<template>
+    <el-table-column :label="label">
+        <template #default="{row}">
+            {{ data(row) }}
+        </template>
+    </el-table-column>
+</template>
+
+<script setup>
+const {props, separator} = defineProps({
+    label: String,
+    props: Array,
+    separator: {
+        type: String,
+        default: '/',
+    },
+    linebreak: Boolean,
+})
+
+const data = row => props.map(item => row[item]).join(separator)
+</script>
+
+<style scoped>
+
+</style>
