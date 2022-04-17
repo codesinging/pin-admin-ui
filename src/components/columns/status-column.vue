ad204f7cf7a245a77ec93e7d74afedc4d3c51f85
codesinging <codesinging@gmail.com>
2022-04-17 14:34:34 +0800

    增加状态标签表格列

---
+++ src/components/columns/status-column.vue
@@ -0,0 +1,39 @@
+<template>
+    <el-table-column :label="label" align="center" width="100">
+        <template #default="{row}">
+            <el-tag :type="row[prop] ? trueType : falseType" :effect="effect" :round="round">{{ row[prop] ? trueText : falseText }}</el-tag>
+        </template>
+    </el-table-column>
+</template>
+
+<script setup>
+defineProps({
+    label: String,
+    prop: String,
+    trueText: {
+        type: String,
+        default: '正常',
+    },
+    falseText: {
+        type: String,
+        default: '禁用',
+    },
+    trueType: {
+        type: String,
+        default: 'success',
+    },
+    falseType: {
+        type: String,
+        default: 'danger',
+    },
+    effect: {
+        type: String,
+        default: 'light',
+    },
+    round: Boolean,
+})
+</script>
+
+<style scoped>
+
+</style>
