2467d2a587d5183eeb4bf30bc5cb2fdd1fbfc5ef
codesinging <codesinging@gmail.com>
2022-04-20 21:47:08 +0800

    修改 status-column 组件

--- src/components/columns/status-column.vue
+++ src/components/columns/status-column.vue
@@ -1,13 +1,15 @@
 <template>
     <el-table-column :label="label" align="center" width="100">
         <template #default="{row}">
-            <el-tag :type="row[prop] ? trueType : falseType" :effect="effect" :round="round">{{ row[prop] ? trueText : falseText }}</el-tag>
+            <el-tag v-if="!hideFalseText || isTrue(row)" :type="isTrue(row) ? trueType : falseType" :effect="effect" :round="round">{{ isTrue(row) ? trueText : falseText }}</el-tag>
         </template>
     </el-table-column>
 </template>
 
 <script setup>
-defineProps({
+import {computed} from "vue";
+
+const props = defineProps({
     label: String,
     prop: String,
     trueText: {
@@ -31,7 +33,10 @@ defineProps({
         default: 'light',
     },
     round: Boolean,
+    hideFalseText: Boolean,
 })
+
+const isTrue = row => row[props.prop]
 </script>
 
 <style scoped>
